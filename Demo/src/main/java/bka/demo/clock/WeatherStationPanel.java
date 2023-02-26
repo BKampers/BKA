0f2eb8ca39a54727c1425d41cd926131cd3ce65a
Bart Kampers <github@bartkampers.nl>
2023-02-26 14:41:26 +0100

    Chill needle

--- Demo/src/main/java/bka/demo/clock/WeatherStationPanel.java
+++ Demo/src/main/java/bka/demo/clock/WeatherStationPanel.java
@@ -15,13 +15,17 @@ public class WeatherStationPanel extends JPanel {
     public WeatherStationPanel() {
         renderer.addClockFace(RADIUS, Color.WHITE);
         temperatureMarkers = renderer.addNumberRingRenderer(RADIUS * 0.9, 10, Color.BLUE);
-        temperatureNeedle = renderer.addNeedleRenderer(RADIUS, Color.BLACK, 3f);
+        chillNeedle = renderer.addNeedleRenderer(chillArrow);
+        temperatureNeedle = renderer.addNeedleRenderer(temperatureArrow);
     }
 
     public void setStation(WeatherStation station) {
         if (station != null) {
-            temperatureNeedle.setValue(station.getTemperature());
-            Logger.getLogger(WeatherStationPanel.class.getName()).log(Level.INFO, "{0} {1}", new Object[]{ station.getStationName(), station.getTemperature() });
+            chillNeedle.setValue((station.getChill() != null) ? station.getChill() : (station.getTemperature() != null) ? station.getTemperature() : -60);
+            chillArrow.setPaint((station.getChill() != null) ? Color.BLUE : null);
+            temperatureNeedle.setValue((station.getTemperature() != null) ? station.getTemperature() : -60);
+            temperatureArrow.setPaint((station.getTemperature() != null) ? Color.RED : null);
+            Logger.getLogger(WeatherStationPanel.class.getName()).log(Level.INFO, "{0}: {1}; {2}", new Object[]{ station.getStationName(), station.getTemperature(), station.getChill() });
         }
         repaint();
     }
@@ -31,8 +35,29 @@ public class WeatherStationPanel extends JPanel {
         renderer.paint((Graphics2D) graphics);
     }
 
+    private class ArrowRenderer implements bka.awt.Renderer {
 
+        @Override
+        public void paint(Graphics2D graphics) {
+            graphics.setPaint((paint != null) ? paint : Color.GRAY);
+            graphics.setStroke(new BasicStroke(3f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+            graphics.fillOval(0 - RADIUS / 20, 0 - RADIUS / 20, RADIUS / 10, RADIUS / 10);
+            graphics.drawLine(0, RADIUS / 10, 0, -(RADIUS / 10) * 9);
+            graphics.drawLine(5, -(RADIUS / 10 * 8), 0, -(RADIUS / 10) * 9);
+            graphics.drawLine(-5, -(RADIUS / 10 * 8), 0, -(RADIUS / 10) * 9);
+        }
+
+        public void setPaint(Paint paint) {
+            this.paint = paint;
+        }
+
+        private Paint paint;
+    }
+    
     private final ClockRenderer renderer = new ClockRenderer(new Point(RADIUS, RADIUS), new Scale(-50, 50, -0.4, 0.4));
+    private final ArrowRenderer chillArrow = new ArrowRenderer();
+    private final NeedleRenderer chillNeedle;
+    private final ArrowRenderer temperatureArrow = new ArrowRenderer();
     private final NeedleRenderer temperatureNeedle;
     private final MarkerRingRenderer temperatureMarkers;
 

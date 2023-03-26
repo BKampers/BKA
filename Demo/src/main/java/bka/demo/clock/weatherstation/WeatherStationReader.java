cae355db5a0c9c308dc6efc96bd39779c22c0c2a
Bart Kampers <github@bartkampers.nl>
2023-03-26 13:52:03 +0200

    Lookup tabel data by header

--- Demo/src/main/java/bka/demo/clock/weatherstation/WeatherStationReader.java
+++ Demo/src/main/java/bka/demo/clock/weatherstation/WeatherStationReader.java
@@ -21,20 +21,27 @@ import java.util.stream.*;
 public class WeatherStationReader {
 
     public static Collection<WeatherStation> getStations() throws IOException {
-        final String page = loadPage();
-        final LocalDateTime timestamp = getTimestamp(page);
+        String page = loadPage();
+        LocalDateTime timestamp = getTimestamp(page);
         XmlMapper xmlMapper = new XmlMapper();
         xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        final TableHeadRow tableHead = xmlMapper.readValue(getTableHead(page), TableHead.class).getRows().get(0);
         return xmlMapper.readValue(getTable(page), TableBody.class)
-            .getRows().stream().map(row -> new Station(row, timestamp)).collect(Collectors.toList());
+            .getRows().stream().map(row -> new Station(tableHead, row, timestamp)).collect(Collectors.toList());
     }
 
     private static LocalDateTime getTimestamp(String page) {
         int dateStart = page.indexOf(TIMESTAMP_START_TAG) + TIMESTAMP_START_TAG.length();
         int dateEnd = page.indexOf(TIMESTAMP_END_TAG, dateStart);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIMESTAMP_FORMAT);
-        System.out.println(page.substring(dateStart, dateEnd));
         return LocalDateTime.parse(page.substring(dateStart, dateEnd), formatter);
+    }
+
+    private static String getTableHead(String page) {
+        int tableStart = page.indexOf(TABLE_HEAD_START_TAG);
+        int tableEnd = page.indexOf(TABLE_HEAD_END_TAG, tableStart + TABLE_START_TAG.length()) + TABLE_END_TAG.length();
+        return page.substring(tableStart, tableEnd).replace("<br>", "").replace("&nbsp;", "").replace("&deg;", "\u00b0");
+
     }
 
     private static String getTable(String page) throws IOException {
@@ -56,6 +63,57 @@ public class WeatherStationReader {
         }
         return page.toString();
     }
+
+    @JacksonXmlRootElement(localName = TABLE_HEAD)
+    private static class TableHead {
+
+        public List<TableHeadRow> getRows() {
+            return rows;
+        }
+
+        @JacksonXmlProperty(localName = TABLE_ROW)
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private List<TableHeadRow> rows;
+    }
+
+    private static class TableHeadRow {
+
+        public TableHeadData[] getColumns() {
+            return columns;
+        }
+
+        public void setColumns(TableHeadData[] columns) {
+            this.columns = columns;
+        }
+
+        public int indexOf(String data) {
+            for (int i = 0; i < columns.length; ++i) {
+                if (data.equals(columns[i].getData())) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        @JacksonXmlProperty(localName = TABLE_HEAD_DATA)
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private TableHeadData[] columns;
+    }
+
+    private static class TableHeadData {
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        @JacksonXmlText
+        private String data;
+    }
+
     @JacksonXmlRootElement(localName = TABLE_BODY)
     private static class TableBody {
 
@@ -117,14 +175,15 @@ public class WeatherStationReader {
 
     private static class Station implements WeatherStation {
 
-        Station(TableRow row, LocalDateTime timestamp) {
+        Station(TableHeadRow tableHead, TableRow row, LocalDateTime timestamp) {
+            this.tableHead = Objects.requireNonNull(tableHead);
             this.row = Objects.requireNonNull(row);
             this.timestamp = Objects.requireNonNull(timestamp);
         }
 
         @Override
         public String getStationName() {
-            return column(0);
+            return column(STATION_NAME_HEADER);
         }
 
         @Override
@@ -134,27 +193,27 @@ public class WeatherStationReader {
 
         @Override
         public String getWeatherSummary() {
-            return column(1);
+            return column(WEATHER_SUMMARY_HEADER);
         }
 
         @Override
         public Double getTemperature() {
-            return parseDouble(column(2));
+            return parseDouble(column(TEMPERATURE_HEADER));
         }
 
         @Override
         public Double getChill() {
-            return parseDouble(column(3));
+            return parseDouble(column(CHILL_HEADER));
         }
 
         @Override
         public Double getHumidity() {
-            return parseDouble(column(4));
+            return parseDouble(column(HUMIDITY_HEADER));
         }
 
         @Override
         public Double getWindDirection() {
-            String directionData = column(5);
+            String directionData = column(WIND_DIRECTION_HEADER);
             if (directionData == null) {
                 return null;
             }
@@ -169,22 +228,22 @@ public class WeatherStationReader {
 
         @Override
         public Double getWindSpeed() {
-            return parseDouble(column(6));
+            return parseDouble(column(WIND_SPEED_HEADER));
         }
 
         @Override
         public Double getSquall() {
-            return parseDouble(column(7));
+            return parseDouble(column(SQUALL_HEADER));
         }
 
         @Override
         public Double getVisibility() {
-            return parseDouble(column(8));
+            return parseDouble(column(VISIBILITY_HEADER));
         }
 
         @Override
         public Double getPressure() {
-            return parseDouble(column(9));
+            return parseDouble(column(PRESSURE_HEADER));
         }
 
         private Double parseDouble(String data) {
@@ -194,25 +253,50 @@ public class WeatherStationReader {
             return Double.parseDouble(data);
         }
 
+        private String column(String header) {
+            int index = tableHead.indexOf(header);
+            if (index < 0) {
+                return null;
+            }
+            return column(index);
+        }
+
         private String column(int index) {
             return row.getColumns()[index].getData();
         }
 
+        private final TableHeadRow tableHead;
         private final TableRow row;
         private final LocalDateTime timestamp;
     }
 
     private static final String KNMI_URL = "https://www.knmi.nl/nederland-nu/weer/waarnemingen";
 
+    private static final String TABLE_HEAD = "thead";
     private static final String TABLE_BODY = "tbody";
     private static final String TABLE_ROW = "tr";
+    private static final String TABLE_HEAD_DATA = "th";
     private static final String TABLE_DATA = "td";
     private static final String DATA_CLASS = "class";
 
+    private static final String TABLE_HEAD_START_TAG = "<thead>";
+    private static final String TABLE_HEAD_END_TAG = "</thead>";
     private static final String TABLE_START_TAG = "<tbody>";
     private static final String TABLE_END_TAG = "</tbody>";
     private static final String TIMESTAMP_START_TAG = "<h2>Waarnemingen ";
     private static final String TIMESTAMP_END_TAG = " uur</h2>";
     private static final String TIMESTAMP_FORMAT = "dd MMMM yyyy HH:mm";
+
+    private static final String STATION_NAME_HEADER = "Station";
+    private static final String WEATHER_SUMMARY_HEADER = "Weer";
+    private static final String TEMPERATURE_HEADER = "Temp(\u00b0C)";
+    private static final String CHILL_HEADER = "Chill(\u00b0C)";
+    private static final String HUMIDITY_HEADER = "RV(%)";
+    private static final String WIND_DIRECTION_HEADER = "Wind(bft)";
+    private static final String WIND_SPEED_HEADER = "Wind(m/s)";
+    private static final String SQUALL_HEADER = "Windstoot(km/uur)";
+    private static final String VISIBILITY_HEADER = "Zicht(m)";
+    private static final String PRESSURE_HEADER = "Druk(hPa)";
+
 
 }

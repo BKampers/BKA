2f6e52bb8afd636cc6a11387cd07159dd50e3141
Bart Kampers <github@bartkampers.nl>
2024-03-03 17:33:49 +0100

    Color chooser popup supports recent colors.

--- Swing/src/main/java/bka/swing/popup/ColorChooserPopupModel.java
+++ Swing/src/main/java/bka/swing/popup/ColorChooserPopupModel.java
@@ -5,19 +5,41 @@
 package bka.swing.popup;
 
 import java.awt.*;
+import java.util.*;
 import java.util.function.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
 
 public class ColorChooserPopupModel extends DefaultPopupModel<Color> {
- 
-    
+
+    /**
+     * Create a ColorChooserPopupModel with a new color chooser, having no recent colors
+     *
+     * @param bounds of the popup
+     * @param initialValue initial color
+     * @param onApply call back to perform when a color is chosen
+     */
     public ColorChooserPopupModel(Rectangle bounds, Color initialValue, Consumer<Color> onApply) {
-        super(bounds, initialValue, onApply);
+        this(bounds, initialValue, onApply, new JColorChooser());
     }
-    
-    
+
+    /**
+     * Create a ColorChooserPopupModel with the color chooser that has previously chosen colors for given key, as recent colors.
+     *
+     * @param bounds of the popup
+     * @param initialValue initial color
+     * @param onApply call back to perform when a color is chosen
+     */
+    public ColorChooserPopupModel(Object key, Rectangle bounds, Color initialValue, Consumer<Color> onApply) {
+        this(bounds, initialValue, onApply, CHOOSERS.computeIfAbsent(Objects.requireNonNull(key), chooser -> new JColorChooser()));
+    }
+
+    private ColorChooserPopupModel(Rectangle bounds, Color initialValue, Consumer<Color> onApply, JColorChooser colorChooser) {
+        super(bounds, initialValue, onApply);
+        this.colorChooser = colorChooser;
+    }
+
     @Override
     public Component getComponent() {
         colorChooser.setPreviewPanel(getPreviewPanel());
@@ -25,18 +47,15 @@ public class ColorChooserPopupModel extends DefaultPopupModel<Color> {
         return colorChooser;
     }
 
-
     @Override
     public void bindListener(Runnable whenReady) {
         colorChooser.getSelectionModel().addChangeListener(new ColorChangeListener(whenReady));
     }
 
-
     @Override
     protected Color getNewValue() {
         return colorChooser.getColor();
     }
-
 
     protected JPanel getPreviewPanel() {
         return new JPanel();
@@ -59,6 +78,8 @@ public class ColorChooserPopupModel extends DefaultPopupModel<Color> {
     }
 
 
-    private final JColorChooser colorChooser = new JColorChooser();
+    private final JColorChooser colorChooser;
+
+    private static final Map<Object, JColorChooser> CHOOSERS = new HashMap<>();
 
 }

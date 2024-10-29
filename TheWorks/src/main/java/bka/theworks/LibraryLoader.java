fcd3635eb3b151d4eb1268a2309bdc8a152e6618
Bart Kampers <github@bartkampers.nl>
2024-10-29 19:44:00 +0100

    List counts Local date/time

--- TheWorks/src/main/java/bka/theworks/LibraryLoader.java
+++ TheWorks/src/main/java/bka/theworks/LibraryLoader.java
@@ -8,6 +8,7 @@ import bka.text.parser.sax.*;
 import java.io.*;
 import java.math.*;
 import java.time.*;
+import java.time.format.*;
 import java.util.*;
 import javax.xml.parsers.*;
 import org.xml.sax.*;
@@ -26,21 +27,69 @@ public class LibraryLoader {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         LibraryLoader loader = new LibraryLoader("/Users/bartkampers/iCloud Drive/Bibliotheek.xml");
-        loader.getTracks().stream().sorted(trackSorter()).forEach(track -> System.out.printf("%s: %s (%2d) %s\n", albumArtistOf(track), albumOf(track), trackNumberOf(track), track.get("Name")));
+        //loader.getTracks().stream().sorted(trackSorter()).forEach(track -> System.out.printf("%s: %s (%2d) %s\n", albumArtistOf(track), albumOf(track), trackNumberOf(track), track.get("Name")));
+        loader.listTop2000();
+        loader.listCounts();
     }
 
-    private static Comparator<Map<String, Object>> trackSorter() {
-        return (track1, track2) -> {
-            int sort = compare(uppercase(albumOf(track1)), uppercase(albumOf(track2)));
-            if (sort != 0) {
-                return sort;
+    public void listTop2000() throws ParserConfigurationException, SAXException, IOException {
+        getTracks().stream()
+            .filter(track -> track.get("Play Date UTC") != null)
+            .sorted(LibraryLoader::favoriteCompare)
+            .limit(2000)
+            .forEach(track -> System.out.printf("(%2d %s) %s | %s\n",
+            ((BigInteger) track.get("Play Count")).intValue(),
+            local(playDateOf(track)),
+            track.get("Artist"),
+            track.get("Name")));
+    }
+
+    private static String local(ZonedDateTime dateTime) {
+        return LocalDateTime.ofInstant(dateTime.toInstant(), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public void listCounts() throws ParserConfigurationException, SAXException, IOException {
+        Map<Integer, Integer> map = new TreeMap<>();
+        List<Map<String, Object>> list = new ArrayList<>(getTracks());
+        list.forEach(track -> {
+            int playCount = playCountOf(track).intValue();
+            fillKeys(map, playCount);
+            map.put(playCount, map.get(playCount) + 1);
+        });
+        map.forEach((playCount, count) -> System.out.printf("%2d: %d\n", playCount, count));
+    }
+
+    private static void fillKeys(Map<Integer, Integer> map, int maxKey) {
+        if (!map.containsKey(maxKey)) {
+            map.put(maxKey, 0);
+            if (maxKey > 0) {
+                fillKeys(map, maxKey - 1);
             }
-            sort = compare(discNumberOf(track1), discNumberOf(track2));
-            if (sort != 0) {
-                return sort;
-            }
-            return compare(trackNumberOf(track1), trackNumberOf(track2));
-        };
+        }
+    }
+
+    private static int favoriteCompare(Map<String, Object> track1, Map<String, Object> track2) {
+        int sort = -compare(playCountOf(track1), playCountOf(track2));
+        if (sort != 0) {
+            return sort;
+        }
+        return -compare(playDateOf(track1), playDateOf(track2));
+    }
+
+    private static int trackCompare(Map<String, Object> track1, Map<String, Object> track2) {
+        int sort = compare(uppercase(albumOf(track1)), uppercase(albumOf(track2)));
+        if (sort != 0) {
+            return sort;
+        }
+        return albumCompare(track1, track2);
+    }
+
+    private static int albumCompare(Map<String, Object> track1, Map<String, Object> track2) {
+        int sort = compare(discNumberOf(track1), discNumberOf(track2));
+        if (sort != 0) {
+            return sort;
+        }
+        return compare(trackNumberOf(track1), trackNumberOf(track2));
     }
 
     private static String uppercase(String string) {
@@ -66,8 +115,12 @@ public class LibraryLoader {
         return (String) track.getOrDefault("Sort Album", track.getOrDefault("Album", null));
     }
 
+    private static BigInteger playCountOf(Map<String, Object> track) {
+        return (BigInteger) track.getOrDefault("Play Count", BigInteger.ZERO);
+    }
+
     private static ZonedDateTime playDateOf(Map<String, Object> track) {
-        return (ZonedDateTime) track.get("Paly Date UTC");
+        return (ZonedDateTime) track.get("Play Date UTC");
     }
 
     private static BigInteger trackNumberOf(Map<String, Object> track) {

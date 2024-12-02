8e9a95475340f2e4b891f8249ce041174ed62203
Bart Kampers <github@bartkampers.nl>
2024-12-02 11:22:13 +0100

    SaxStackHandler test for HTML table

--- Text/src/test/java/bka/text/parser/sax/SaxStackHandlerTest.java
+++ Text/src/test/java/bka/text/parser/sax/SaxStackHandlerTest.java
@@ -44,40 +44,57 @@ public class SaxStackHandlerTest {
         assertEquals((long) 8, ((List) handler.getObjects().get(0)).get(3));
     }
 
+    @Test
+    public void testHtmlTable() throws SAXException, ParserConfigurationException, IOException {
+        SaxStackHandler handler = new SaxStackHandler(getHtmlTableModel());
+        createSaxParser().parse("src/test/resources/xml/html-table.xml", handler);
+        Map table = (Map) handler.getObjects().get(0);
+        assertEquals(
+            List.of(
+                List.of("Station", "Weer", "Temp (°C)", "Chill (°C)", "RV (%)", "Wind (bft)", "Wind (m/s)", "Windstoot (km/uur)", "Zicht (m)", "Druk (hPa)")),
+            table.get("Headers"));
+        assertEquals(
+            List.of(
+                List.of("Lauwersoog", "", "6.9", "3.3", "65", "ZZO 4", "6", "29", "", ""),
+                List.of("Terschelling", "", "6.3", "2.5", "78", "ZZO 4", "6", "31", "31000", "1015.0"),
+                List.of("Vlieland", "zwaar bewolkt", "6.6", "2.0", "75", "ZZO 5", "9", "40", "35000", "1014.4")),
+            table.get("Rows"));
+    }
+
     private static SAXParser createSaxParser() throws SAXException, ParserConfigurationException {
         return SAXParserFactory.newInstance().newSAXParser();
     }
 
     private static SaxStackHandler.Model getArrayModel() {
-        return (localName, qualifiedName, attribuets, children, characters) -> {
-            if (!localName.isEmpty()) {
-                throw new IllegalArgumentException("Unsupported local name: " + localName);
-            }
-            return switch (qualifiedName) {
-                case "root" ->
-                    children.getList("element");
-                case "element" ->
-                    characters;
-                default ->
-                    throw new IllegalArgumentException("Cannot create element for qualified name '" + qualifiedName + "'");
-            };
+        return (localName, qualifiedName, attribuets, children, characters) -> switch (qualifiedName) {
+            case "root" ->
+                children.getList("element");
+            case "element" ->
+                characters;
+            default ->
+                throw new IllegalArgumentException(unsupportedQualifiedName(qualifiedName));
         };
     }
 
     private static SaxStackHandler.Model getBookshlefModel() {
-        return (localName, qualifiedName, attributes, children, characters) -> switch (qualifiedName) {
-            case "shelf" ->
-                children.getList("book");
-            case "book" ->
-                createBook(children);
-            case "authors" ->
-                children;
-            case "title", "author" ->
-                characters;
-            case "release" ->
-                Integer.parseInt(characters);
-            default ->
-                throw new IllegalArgumentException("Cannot create element for qualified name '" + qualifiedName + "'");
+        return (localName, qualifiedName, attributes, children, characters) -> {
+            if (!localName.isEmpty()) {
+                throw new IllegalArgumentException(unsupportedLocalName(localName));
+            }
+            return switch (qualifiedName) {
+                case "shelf" ->
+                    children.getList("book");
+                case "book" ->
+                    createBook(children);
+                case "authors" ->
+                    children;
+                case "title", "author" ->
+                    characters;
+                case "release" ->
+                    Integer.parseInt(characters);
+                default ->
+                    throw new IllegalArgumentException(unsupportedQualifiedName(qualifiedName));
+            };
         };
     }
 
@@ -90,7 +107,8 @@ public class SaxStackHandlerTest {
     }
 
     private static SaxStackHandler.Model getIntegerModel() {
-        return (localName, qualifiedName, attributes, children, characters) -> switch (localName) {
+        return (localName, qualifiedName, attributes, children, characters)
+            -> switch (localName) {
             case "" ->
                 switch (qualifiedName) {
                     case "integers" ->
@@ -98,10 +116,10 @@ public class SaxStackHandlerTest {
                     case "integer" ->
                         getInteger(attributes.getValue("width"), characters);
                     default ->
-                        throw new IllegalArgumentException("Cannot create element for qualified name '" + qualifiedName + "'");
+                        throw new IllegalArgumentException(unsupportedQualifiedName(qualifiedName));
                 };
             default ->
-                throw new IllegalArgumentException("Unsupported local name '" + localName + "'");
+                throw new IllegalArgumentException(unsupportedLocalName(localName));
         };
     }
 
@@ -116,8 +134,55 @@ public class SaxStackHandlerTest {
             case "64" ->
                 Long.valueOf(characters);
             default ->
-                throw new IllegalArgumentException("Unsupported width: '" + width + "'");
+                throw new IllegalArgumentException("Unsupported width: " + quoted(width));
         };
+    }
+
+    private static SaxStackHandler.Model getHtmlTableModel() {
+        return (localName, qualifiedName, attributes, children, characters) -> switch (localName) {
+            case "" ->
+                switch (qualifiedName) {
+                    case "table" ->
+                        getTable(children);
+                    case "thead", "tbody" ->
+                        children.getList("tr");
+                    case "tr" ->
+                        getColumns(children);
+                    case "th", "td" ->
+                        characters.replace(NBSP, "").trim();
+                    default ->
+                        throw new IllegalArgumentException(unsupportedQualifiedName(qualifiedName));
+                };
+            default ->
+                throw new IllegalArgumentException(unsupportedLocalName(localName));
+        };
+    }
+
+    private static Map<String, List<String>> getTable(SaxStackHandler.Children children) {
+        return Map.of(
+            "Headers", children.getElement("thead"),
+            "Rows", children.getElement("tbody"));
+    }
+
+    private static List<String> getColumns(SaxStackHandler.Children children) {
+        List<String> columns = children.getList("td");
+        if (columns.isEmpty()) {
+            return children.getList("th");
+        }
+        return columns;
+    }
+
+
+    private static String unsupportedLocalName(String localName) {
+        return "Unsupported local name " + quoted(localName);
+    }
+
+    private static String unsupportedQualifiedName(String qualifiedName) {
+        return "Cannot create element for qualified name " + quoted(qualifiedName);
+    }
+
+    private static String quoted(String string) {
+        return "'" + string + "'";
     }
 
     private static class Book {
@@ -144,5 +209,7 @@ public class SaxStackHandlerTest {
         private final List<String> authors;
         private final int release;
     }
+
+    private static final String NBSP = "\u00a0";
 
 }

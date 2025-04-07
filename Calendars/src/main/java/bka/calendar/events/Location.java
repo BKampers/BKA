8e79c3debb6f4b90ba65bec27551ed616b495977
Bart Kampers <github@bartkampers.nl>
2025-04-07 20:55:10 +0200

    Redundant constructors removed

--- Calendars/src/main/java/bka/calendar/events/Location.java
+++ Calendars/src/main/java/bka/calendar/events/Location.java
@@ -4,8 +4,6 @@
 
 package bka.calendar.events;
 
-import java.math.*;
-
 /**
  * Simple VO class to store latitude/longitude information.
  */
@@ -26,26 +24,6 @@ public class Location {
         }
         this.latitude = latitude;
         this.longitude = longitude;
-    }
-
-    /**
-     * Creates a new instance of <code>Location</code> with the given parameters.
-     *
-     * @param latitude the latitude, in degrees, of this location. North latitude is positive, south negative.
-     * @param longitude the longitude, in degrees of this location. East longitude is positive, west negative.
-     */
-    public Location(BigDecimal latitude, BigDecimal longitude) {
-        this(latitude.doubleValue(), longitude.doubleValue());
-    }
-
-    /**
-     * Creates a new instance of <code>Location</code> with the given parameters.
-     *
-     * @param latitude the latitude, in degrees, of this location. North latitude is positive, south negative.
-     * @param longitude the longitude, in degrees of this location. East longitude is positive, west negative.
-     */
-    public Location(String latitude, String longitude) {
-        this(Double.parseDouble(latitude), Double.parseDouble(longitude));
     }
 
     /**

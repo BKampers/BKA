ee167868e1fc6d30b4ef2ac73e716688d4e13978
Bart Kampers <github@bartkampers.nl>
2025-05-06 15:45:40 +0200

    Tertiary intercardinal directions

--- Text/src/main/java/bka/text/cardinal/CardinalNumberFormat.java
+++ Text/src/main/java/bka/text/cardinal/CardinalNumberFormat.java
@@ -6,40 +6,61 @@ package bka.text.cardinal;
 
 import java.text.*;
 import java.util.*;
+import java.util.stream.*;
 
 
 public class CardinalNumberFormat extends NumberFormat {
+
+    public static FieldPosition cardinalFieldPostion() {
+        return new FieldPosition(CARDINAL);
+    }
+
+    public static FieldPosition interCardinalFieldPostion() {
+        return new FieldPosition(INTERCARDINAL);
+    }
+
+    public static FieldPosition secondaryInterCardinalFieldPostion() {
+        return new FieldPosition(SECONDARY_INTERCARDINAL);
+    }
+
+    public static FieldPosition tertiaryInterCardinalFieldPostion() {
+        return new FieldPosition(TERTIARY_INTERCARDINAL);
+    }
 
     public CardinalNumberFormat() {
         this(Locale.getDefault());
     }
 
     public CardinalNumberFormat(Locale locale) {
-        ResourceBundle bundle = ResourceBundle.getBundle(CardinalNumberFormat.class.getSimpleName(), locale);
-        for (String key : KEYS) {
-            directions.add(bundle.getString(key));
-        }
+        bundle = ResourceBundle.getBundle(CardinalNumberFormat.class.getSimpleName(), locale);
     }
 
     @Override
-    public StringBuffer format(long degrees, StringBuffer toAppendTo, FieldPosition position) {
-        return format((double) degrees, toAppendTo, position);
+    public StringBuffer format(long degrees, StringBuffer toAppendTo, FieldPosition fieldPosition) {
+        return toAppendTo.append(getDirection(degrees, fieldPosition));
     }
 
     @Override
-    public StringBuffer format(double degrees, StringBuffer toAppendTo, FieldPosition position) {
-        return toAppendTo.append(directions.get(index(degrees)));
-    }
-
-    private int index(double degrees) {
+    public StringBuffer format(double degrees, StringBuffer toAppendTo, FieldPosition fieldPosition) {
         if (!Double.isFinite(degrees)) {
-            throw new IllegalArgumentException("Parameter degrees must be finite");
+            throw new IllegalArgumentException("degrees must be finite");
         }
-        return ((int) Math.round(normalized(degrees) / INTERVAL)) % directions.size();
+        return toAppendTo.append(getDirection(degrees, fieldPosition));
     }
 
-    private static double normalized(double degrees) {
-        return degrees - Math.floor(degrees / MAX_DEGREES) * MAX_DEGREES;
+    private String getDirection(double degrees, FieldPosition fieldPosition) {
+        List<String> directions = directions(field(fieldPosition));
+        return directions.get(index(degrees, directions));
+    }
+
+    private int field(FieldPosition position) {
+        int field = position.getField();
+        return (field < CARDINAL || TERTIARY_INTERCARDINAL < field) ? SECONDARY_INTERCARDINAL : field;
+    }
+
+    private int index(double degrees, List<String> directions) {
+        int index = (int) (Math.round(degrees / (MAX_DEGREES / directions.size())) % directions.size());
+        return (index < 0) ? index + directions.size() : index;
     }
 
     @Override
@@ -48,6 +69,7 @@ public class CardinalNumberFormat extends NumberFormat {
             position.setErrorIndex(source.length());
             return null;
         }
+        List<String> directions = directions(TERTIARY_INTERCARDINAL);
         Optional<String> direction = directions.stream()
             .sorted(longestFirst())
             .filter(d -> source.startsWith(d, position.getIndex()))
@@ -57,18 +79,38 @@ public class CardinalNumberFormat extends NumberFormat {
             return null;
         }
         position.setIndex(position.getIndex() + direction.get().length());
-        return directions.indexOf(direction.get()) * INTERVAL;
+        return directions.indexOf(direction.get()) * (MAX_DEGREES / directions.size());
     }
 
     private static Comparator<String> longestFirst() {
         return (string1, string2) -> string2.length() - string1.length();
     }
 
+    private List<String> directions(int field) {
+        return directionsCache.computeIfAbsent(field, f -> switch (field) {
+            case TERTIARY_INTERCARDINAL ->
+                Stream.of(KEYS).map(bundle::getString).collect(Collectors.toList());
+            case SECONDARY_INTERCARDINAL ->
+                Stream.of(KEYS).filter(key -> !key.contains("b")).map(bundle::getString).collect(Collectors.toList());
+            default ->
+                Stream.of(KEYS).filter(key -> key.length() <= field).map(bundle::getString).collect(Collectors.toList());
+        });
+    }
 
-    private final List<String> directions = new ArrayList<>();
+    private final ResourceBundle bundle;
+    private final Map<Integer, List<String>> directionsCache = new HashMap<>();
 
-    private static final String[] KEYS = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" };
+    private static final String[] KEYS = {
+        "N", "NbE", "NNE", "NEbN", "NE", "NEbE", "ENE", "EbN",
+        "E", "EbS", "ESE", "SEbE", "SE", "SEbS", "SSE", "SbE",
+        "S", "SbW", "SSW", "SWbS", "SW", "SWbW", "WSW", "WbS",
+        "W", "WbN", "WNW", "NWbW", "NW", "NWbN", "NNW", "NbW" };
+
     private static final double MAX_DEGREES = 360;
-    private static final double INTERVAL = MAX_DEGREES / KEYS.length;
+
+    private static final int CARDINAL = 1;
+    private static final int INTERCARDINAL = 2;
+    private static final int SECONDARY_INTERCARDINAL = 3;
+    private static final int TERTIARY_INTERCARDINAL = 4;
 
 }
--- Text/src/test/java/bka/text/cardinal/CardinalNumberFormatTest.java
+++ Text/src/test/java/bka/text/cardinal/CardinalNumberFormatTest.java
@@ -6,6 +6,7 @@ package bka.text.cardinal;
 
 import java.text.*;
 import java.util.*;
+import java.util.function.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,6 +19,45 @@ public class CardinalNumberFormatTest {
     @BeforeAll
     public static void initialize() {
         Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @Test
+    public void testFormatWithField() {
+        testBoundaries(
+            new String[]{ "N", "E", "S", "W" },
+            CardinalNumberFormat.cardinalFieldPostion());
+        testBoundaries(
+            new String[]{ "N", "NE", "E", "SE", "S", "SW", "W", "NW" },
+            CardinalNumberFormat.interCardinalFieldPostion());
+        testBoundaries(
+            new String[]{ "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" },
+            CardinalNumberFormat.secondaryInterCardinalFieldPostion());
+        testBoundaries(
+            new String[]{
+                "N", "NbE", "NNE", "NEbN", "NE", "NEbE", "ENE", "EbN",
+                "E", "EbS", "ESE", "SEbE", "SE", "SEbS", "SSE", "SbE",
+                "S", "SbW", "SSW", "SWbS", "SW", "SWbW", "WSW", "WbS",
+                "W", "WbN", "WNW", "NWbW", "NW", "NWbN", "NNW", "NbW" },
+            CardinalNumberFormat.tertiaryInterCardinalFieldPostion());
+    }
+
+    private void testBoundaries(String[] expected, FieldPosition fieldPosition) {
+        CardinalNumberFormat format = new CardinalNumberFormat();
+        final double interval = 360d / expected.length;
+        final double precision = 0.001;
+        double degrees = 3 * -360 - 0.5 * interval;
+        while (degrees < 360 * 3) {
+            for (String direction : expected) {
+                assertEquals(direction, format.format(degrees, new StringBuffer(), fieldPosition).toString(), message(degrees, fieldPosition));
+                degrees += interval;
+                assertEquals(direction, format.format(degrees - precision, new StringBuffer(), fieldPosition).toString(), message(degrees, fieldPosition));
+            }
+        }
+    }
+
+    private static Supplier<String> message(double degrees, FieldPosition fieldPosition) {
+        double normalized = degrees - Math.floor(degrees / 360) * 360;
+        return () -> "degrees = " + Double.toString(degrees) + "; (" + normalized + ") field = " + fieldPosition.getField();
     }
 
     @Test
@@ -48,57 +88,86 @@ public class CardinalNumberFormatTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "de", "en", "nl", "?" })
+    @ValueSource(strings = { "de", "en", "nl" })
     public void testFormatLocale(String locale) {
+        FieldPosition fieldPosition = new FieldPosition(4);
         CardinalNumberFormat format = new CardinalNumberFormat(Locale.of(locale));
-        assertEquals(translated("N", locale), format.format(0.0));
-        assertEquals(translated("NNE", locale), format.format(22.5));
-        assertEquals(translated("NE", locale), format.format(45.0));
-        assertEquals(translated("ENE", locale), format.format(67.5));
-        assertEquals(translated("E", locale), format.format(90.0));
-        assertEquals(translated("ESE", locale), format.format(112.5));
-        assertEquals(translated("SE", locale), format.format(135.0));
-        assertEquals(translated("SSE", locale), format.format(157.5));
-        assertEquals(translated("S", locale), format.format(180.0));
-        assertEquals(translated("SSW", locale), format.format(202.5));
-        assertEquals(translated("SW", locale), format.format(225.0));
-        assertEquals(translated("WSW", locale), format.format(247.5));
-        assertEquals(translated("W", locale), format.format(270.0));
-        assertEquals(translated("WNW", locale), format.format(292.5));
-        assertEquals(translated("NW", locale), format.format(315.0));
-        assertEquals(translated("NNW", locale), format.format(337.5));
+        assertEquals(translated("N", locale), format.format(0.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NbE", locale), format.format(11.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NNE", locale), format.format(22.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NEbN", locale), format.format(33.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NE", locale), format.format(45.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NEbE", locale), format.format(56.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("ENE", locale), format.format(67.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("EbN", locale), format.format(78.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("E", locale), format.format(90.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("EbS", locale), format.format(101.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("ESE", locale), format.format(112.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SEbE", locale), format.format(123.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SE", locale), format.format(135.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SEbS", locale), format.format(146.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SSE", locale), format.format(157.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SbE", locale), format.format(168.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("S", locale), format.format(180.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SbW", locale), format.format(191.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SSW", locale), format.format(202.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SWbS", locale), format.format(213.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SW", locale), format.format(225.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("SWbW", locale), format.format(236.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("WSW", locale), format.format(247.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("WbS", locale), format.format(258.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("W", locale), format.format(270.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("WbN", locale), format.format(281.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("WNW", locale), format.format(292.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NWbW", locale), format.format(303.75, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NW", locale), format.format(315.0, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NWbN", locale), format.format(326.25, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NNW", locale), format.format(337.5, new StringBuffer(), fieldPosition).toString());
+        assertEquals(translated("NbW", locale), format.format(348.75, new StringBuffer(), fieldPosition).toString());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "de", "en", "nl", "?" })
+    @ValueSource(strings = { "de", "en", "nl" })
     public void testParseLocale(String locale) throws ParseException {
         CardinalNumberFormat format = new CardinalNumberFormat(Locale.of(locale));
         assertEquals(0.0, format.parse(translated("N", locale)));
+        assertEquals(11.25, format.parse(translated("NbE", locale)));
         assertEquals(22.5, format.parse(translated("NNE", locale)));
+        assertEquals(33.75, format.parse(translated("NEbN", locale)));
         assertEquals(45.0, format.parse(translated("NE", locale)));
+        assertEquals(56.25, format.parse(translated("NEbE", locale)));
         assertEquals(67.5, format.parse(translated("ENE", locale)));
+        assertEquals(78.75, format.parse(translated("EbN", locale)));
         assertEquals(90.0, format.parse(translated("E", locale)));
+        assertEquals(101.25, format.parse(translated("EbS", locale)));
         assertEquals(112.5, format.parse(translated("ESE", locale)));
+        assertEquals(123.75, format.parse(translated("SEbE", locale)));
         assertEquals(135.0, format.parse(translated("SE", locale)));
+        assertEquals(146.25, format.parse(translated("SEbS", locale)));
         assertEquals(157.5, format.parse(translated("SSE", locale)));
+        assertEquals(168.75, format.parse(translated("SbE", locale)));
         assertEquals(180.0, format.parse(translated("S", locale)));
+        assertEquals(191.25, format.parse(translated("SbW", locale)));
         assertEquals(202.5, format.parse(translated("SSW", locale)));
+        assertEquals(213.75, format.parse(translated("SWbS", locale)));
         assertEquals(225.0, format.parse(translated("SW", locale)));
+        assertEquals(236.25, format.parse(translated("SWbW", locale)));
         assertEquals(247.5, format.parse(translated("WSW", locale)));
+        assertEquals(258.75, format.parse(translated("WbS", locale)));
         assertEquals(270.0, format.parse(translated("W", locale)));
+        assertEquals(281.25, format.parse(translated("WbN", locale)));
         assertEquals(292.5, format.parse(translated("WNW", locale)));
+        assertEquals(303.75, format.parse(translated("NWbW", locale)));
         assertEquals(315.0, format.parse(translated("NW", locale)));
+        assertEquals(326.25, format.parse(translated("NWbN", locale)));
         assertEquals(337.5, format.parse(translated("NNW", locale)));
+        assertEquals(348.75, format.parse(translated("NbW", locale)));
     }
 
     @Test
     public void testParseWithPosition() throws ParseException {
-        NumberFormat format = new DecimalFormat();
-        ParsePosition position = new ParsePosition(1);
-        assertEquals(1.2, format.parse("X1.2X", position));
-        assertEquals(4, position.getIndex());
-        format = new CardinalNumberFormat();
-        position = new ParsePosition(0);
+        CardinalNumberFormat format = new CardinalNumberFormat();
+        ParsePosition position = new ParsePosition(0);
         assertEquals(0.0, format.parse("N_", position));
         assertEquals(1, position.getIndex());
         assertEquals(-1, position.getErrorIndex());

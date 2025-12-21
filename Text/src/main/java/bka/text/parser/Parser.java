3f4a39bcf23850a585ba1beb89bc3391a5d095d6
Bart Kampers <github@bartkampers.nl>
2025-12-21 13:56:15 +0100

    Engine inner class introduced to make Parser immutable.

--- Text/src/main/java/bka/text/parser/Parser.java
+++ Text/src/main/java/bka/text/parser/Parser.java
@@ -23,202 +23,211 @@ public class Parser {
 
     public Node parse(String sourceCode, String startSymbol) {
         if (!grammar.getRules().getNonterminals().contains(startSymbol)) {
-            throw new IllegalArgumentException("Invalid symbol: " + startSymbol);
+            throw new IllegalArgumentException("Invalid start symbol: '" + startSymbol + "'");
         }
-        source = sourceCode;
-        matchers.clear();
-        skips.clear();
-        Node tree = createTreeNode(0, startSymbol);
-        if (tree.getError().isPresent()) {
+        return new Engine(sourceCode).parse(startSymbol);
+    }
+
+
+    private class Engine {
+
+        private Engine(String source) {
+            this.source = Objects.requireNonNull(source);
+        }
+
+        public Node parse(String startSymbol) {
+            Node tree = createTreeNode(0, startSymbol);
+            if (tree.getError().isPresent()) {
+                return tree;
+            }
+            return validateRemainder(tree);
+        }
+
+        private Node validateRemainder(Node tree) {
+            int index = skipWhitespaceAndComment(tree.getEnd());
+            if (index < 0) {
+                return createErrorTree(tree, UNTERMINATED_COMMENT);
+            }
+            if (index < source.length()) {
+                return createErrorTree(tree, String.format(UNPARSABLE_CODE_AFTER_SYMBOL, tree.getSymbol()));
+            }
             return tree;
         }
-        return validateRemainder(tree);
-    }
 
-    private Node validateRemainder(Node tree) {
-        int index = skipWhitespaceAndComment(tree.getEnd());
-        if (index < 0) {
-            return createErrorTree(tree, UNTERMINATED_COMMENT);
+        private Node createErrorTree(Node tree, String message) {
+            Node error = new Node(source, "", tree.getEnd(), message);
+            List<Node> children = new ArrayList<>(tree.getChildren());
+            children.add(error);
+            return new Node(source, tree.getSymbol(), tree.getStart(), children, message);
         }
-        if (index < source.length()) {
-            return createErrorTree(tree, String.format(UNPARSABLE_CODE_AFTER_SYMBOL, tree.getSymbol()));
-        }
-        return tree;
-    }
 
-    private Node createErrorTree(Node tree, String message) {
-        Node error = new Node(source, "", tree.getEnd(), message);
-        List<Node> children = new ArrayList<>(tree.getChildren());
-        children.add(error);
-        return new Node(source, tree.getSymbol(), tree.getStart(), children, message);
-    }
-
-    private List<Node> buildTree(int index, String nonterminal) {
-        int headIndex = skipWhitespaceAndComment(index);
-        if (headIndex < 0) {
-            return List.of(new Node(source, nonterminal, index, UNTERMINATED_COMMENT));
-        }
-        List<Node> resolution = resolve(nonterminal, headIndex);
-        if (findError(resolution).isPresent()) {
-            return resolution;
-        }
-        List<Node> recursiveResolution = resolveLeftRecursive(nonterminal, headIndex, resolution);
-        return (recursiveResolution != null) ? recursiveResolution : resolution;
-    }
-
-    private List<Node> resolve(String nonterminal, int headIndex) {
-        Node errorNode = null;
-        List<Node> errorTree = null;
-        for (Sentential sentential : grammar.getSententials(nonterminal).stream().filter(isLeftRecursive(nonterminal).negate()).toList()) {
-            List<Node> resolution = resolve(headIndex, sentential.getSymbols());
-            Optional<Node> error = findError(resolution);
-            if (error.isEmpty()) {
+        private List<Node> buildTree(int index, String nonterminal) {
+            int headIndex = skipWhitespaceAndComment(index);
+            if (headIndex < 0) {
+                return List.of(new Node(source, nonterminal, index, UNTERMINATED_COMMENT));
+            }
+            List<Node> resolution = resolve(nonterminal, headIndex);
+            if (findError(resolution).isPresent()) {
                 return resolution;
             }
-            if (errorNode == null || error.get().getStart() >= errorNode.getStart()) {
-                errorNode = error.get();
-                errorTree = resolution;
-            }
+            List<Node> recursiveResolution = resolveLeftRecursive(nonterminal, headIndex, resolution);
+            return (recursiveResolution != null) ? recursiveResolution : resolution;
         }
-        return errorTree;
-    }
 
-    private List<Node> resolveLeftRecursive(String nonterminal, int headIndex, List<Node> resolutionHead) {
-        List<Sentential> leftRecursiveSententials = grammar.getSententials(nonterminal).stream().filter(isLeftRecursive(nonterminal)).toList();
-        if (leftRecursiveSententials.isEmpty()) {
+        private List<Node> resolve(String nonterminal, int headIndex) {
+            Node errorNode = null;
+            List<Node> errorTree = null;
+            for (Sentential sentential : grammar.getSententials(nonterminal).stream().filter(isLeftRecursive(nonterminal).negate()).toList()) {
+                List<Node> resolution = resolve(headIndex, sentential.getSymbols());
+                Optional<Node> error = findError(resolution);
+                if (error.isEmpty()) {
+                    return resolution;
+                }
+                if (errorNode == null || error.get().getStart() >= errorNode.getStart()) {
+                    errorNode = error.get();
+                    errorTree = resolution;
+                }
+            }
+            return errorTree;
+        }
+
+        private List<Node> resolveLeftRecursive(String nonterminal, int headIndex, List<Node> resolutionHead) {
+            List<Sentential> leftRecursiveSententials = grammar.getSententials(nonterminal).stream().filter(isLeftRecursive(nonterminal)).toList();
+            if (leftRecursiveSententials.isEmpty()) {
+                return null;
+            }
+            int tailIndex = skipWhitespaceAndComment(resolutionHead.getLast().getEnd());
+            if (tailIndex < 0) {
+                return List.of(new Node(source, nonterminal, resolutionHead.getLast().getEnd(), UNTERMINATED_COMMENT));
+            }
+            for (Sentential sentential : leftRecursiveSententials) {
+                List<Node> resolutionTail = resolve(tailIndex, tail(sentential));
+                if (findError(resolutionTail).isEmpty()) {
+                    List<Node> resolution = new ArrayList<>();
+                    Node head = new Node(source, nonterminal, headIndex, resolutionHead);
+                    resolution.add(head);
+                    resolution.addAll(resolutionTail);
+                    return resolution;
+                }
+            }
             return null;
         }
-        int tailIndex = skipWhitespaceAndComment(resolutionHead.getLast().getEnd());
-        if (tailIndex < 0) {
-            return List.of(new Node(source, nonterminal, resolutionHead.getLast().getEnd(), UNTERMINATED_COMMENT));
+
+        private static Predicate<Sentential> isLeftRecursive(String nonterminal) {
+            return sentential -> Grammar.isLeftRecursiveRule(nonterminal, sentential);
         }
-        for (Sentential sentential : leftRecursiveSententials) {
-            List<Node> resolutionTail = resolve(tailIndex, tail(sentential));
-            if (findError(resolutionTail).isEmpty()) {
-                List<Node> resolution = new ArrayList<>();
-                Node head = new Node(source, nonterminal, headIndex, resolutionHead);
-                resolution.add(head);
-                resolution.addAll(resolutionTail);
-                return resolution;
+
+        private static List<String> tail(Sentential sentential) {
+            return sentential.getSymbols().stream().skip(1).toList();
+        }
+
+        private List<Node> resolve(int index, List<String> symbols) {
+            List<Node> resolution = new ArrayList<>();
+            int sourceIndex = index;
+            for (String symbol : symbols) {
+                Node node = createNode(sourceIndex, symbol);
+                resolution.add(node);
+                if (node.getError().isPresent()) {
+                    return resolution;
+                }
+                sourceIndex = skipWhitespaceAndComment(node.getEnd());
+                if (sourceIndex < 0) {
+                    resolution.add(new Node(source, "", node.getEnd(), UNTERMINATED_COMMENT));
+                    return resolution;
+                }
+            }
+            return resolution;
+        }
+
+        private Node createNode(int index, String symbol) {
+            if (grammar.getRules().getNonterminals().contains(symbol)) {
+                return createTreeNode(index, symbol);
+            }
+            return createMatchNode(symbol, index);
+        }
+
+        private Node createTreeNode(int index, String symbol) {
+            List<Node> children = buildTree(index, symbol);
+            Optional<Node> error = findError(children);
+            if (error.isPresent()) {
+                return new Node(source, symbol, index, children, error.get().getError().get());
+            }
+            return new Node(source, symbol, index, children);
+        }
+
+        private Node createMatchNode(String symbol, int index) {
+            Matcher matcher = matchers.computeIfAbsent(symbol, this::createMatcher);
+            if (matcher.find(index) && matcher.start() == index) {
+                return new Node(source, symbol, index, matcher.end());
+            }
+            return new Node(source, symbol, index, NO_MATCH);
+        }
+
+        private static Optional<Node> findError(List<Node> nodes) {
+            return nodes.stream().filter(node -> node.getError().isPresent()).findAny();
+        }
+
+        private Matcher createMatcher(String symbol) {
+            return Pattern.compile(symbol, Pattern.CASE_INSENSITIVE).matcher(source);
+        }
+
+        private int skipWhitespaceAndComment(int startIndex) {
+            return skips.computeIfAbsent(startIndex, this::skip);
+        }
+
+        private int skip(int startIndex) {
+            int endIndex = startIndex;
+            for (;;) {
+                endIndex = skipWhitespace(endIndex);
+                Optional<CommentBrackets> brackets = findCommentBrackets(endIndex);
+                if (brackets.isEmpty()) {
+                    return endIndex;
+                }
+                endIndex = skipComment(endIndex, brackets.get());
+                if (endIndex < 0) {
+                    return -1;
+                }
             }
         }
-        return null;
-    }
 
-    private static Predicate<Sentential> isLeftRecursive(String nonterminal) {
-        return sentential -> Grammar.isLeftRecursiveRule(nonterminal, sentential);
-    }
-
-    private static List<String> tail(Sentential sentential) {
-        return sentential.getSymbols().stream().skip(1).toList();
-    }
-
-    private List<Node> resolve(int index, List<String> symbols) {
-        List<Node> resolution = new ArrayList<>();
-        int sourceIndex = index;
-        for (String symbol : symbols) {
-            Node node = createNode(sourceIndex, symbol);
-            resolution.add(node);
-            if (node.getError().isPresent()) {
-                return resolution;
+        private int skipWhitespace(int startIndex) {
+            int index = startIndex;
+            while (index < source.length() && Character.isWhitespace(source.charAt(index))) {
+                index++;
             }
-            sourceIndex = skipWhitespaceAndComment(node.getEnd());
-            if (sourceIndex < 0) {
-                resolution.add(new Node(source, "", node.getEnd(), UNTERMINATED_COMMENT));
-                return resolution;
-            }
+            return index;
         }
-        return resolution;
-    }
 
-    private Node createNode(int index, String symbol) {
-        if (grammar.getRules().getNonterminals().contains(symbol)) {
-            return createTreeNode(index, symbol);
+        private int skipComment(int index, CommentBrackets brackets) {
+            return (brackets.isBlockComment())
+                ? skipBlockComment(index, brackets)
+                : skipLineComment(index, brackets);
         }
-        return createMatchNode(symbol, index);
-    }
 
-    private Node createTreeNode(int index, String symbol) {
-        List<Node> children = buildTree(index, symbol);
-        Optional<Node> error = findError(children);
-        if (error.isPresent()) {
-            return new Node(source, symbol, index, children, error.get().getError().get());
-        }
-        return new Node(source, symbol, index, children);
-    }
-
-    private Node createMatchNode(String symbol, int index) {
-        Matcher matcher = matchers.computeIfAbsent(symbol, this::createMatcher);
-        if (matcher.find(index) && matcher.start() == index) {
-            return new Node(source, symbol, index, matcher.end());
-        }
-        return new Node(source, symbol, index, NO_MATCH);
-    }
-
-    private static Optional<Node> findError(List<Node> nodes) {
-        return nodes.stream().filter(node -> node.getError().isPresent()).findAny();
-    }
-
-    private Matcher createMatcher(String symbol) {
-        return Pattern.compile(symbol, Pattern.CASE_INSENSITIVE).matcher(source);
-    }
-
-    private int skipWhitespaceAndComment(int startIndex) {
-        return skips.computeIfAbsent(startIndex, this::skip);
-    }
-
-    private int skip(int startIndex) {
-        int endIndex = startIndex;
-        for (;;) {
-            endIndex = skipWhitespace(endIndex);
-            Optional<CommentBrackets> brackets = findCommentBrackets(endIndex);
-            if (brackets.isEmpty()) {
-                return endIndex;
-            }
-            endIndex = skipComment(endIndex, brackets.get());
+        private int skipBlockComment(int startIndex, CommentBrackets brackets) {
+            int endIndex = source.indexOf(brackets.getEnd(), startIndex + brackets.getStart().length());
             if (endIndex < 0) {
                 return -1;
             }
+            return endIndex + brackets.getEnd().length();
         }
-    }
 
-    private int skipWhitespace(int startIndex) {
-        int index = startIndex;
-        while (index < source.length() && Character.isWhitespace(source.charAt(index))) {
-            index++;
+        private int skipLineComment(int startIndex, CommentBrackets brackets) {
+            int endIndex = source.indexOf('\n', startIndex + brackets.getStart().length());
+            if (endIndex < 0) {
+                return source.length();
+            }
+            return endIndex + 1;
         }
-        return index;
-    }
 
-    private int skipComment(int index, CommentBrackets brackets) {
-        return (brackets.isBlockComment())
-            ? skipBlockComment(index, brackets)
-            : skipLineComment(index, brackets);
-    }
-
-    private int skipBlockComment(int startIndex, CommentBrackets brackets) {
-        int endIndex = source.indexOf(brackets.getEnd(), startIndex + brackets.getStart().length());
-        if (endIndex < 0) {
-            return -1;
+        private Optional<CommentBrackets> findCommentBrackets(int index) {
+            return grammar.getCommentBrackets().stream().filter(brackets -> source.startsWith(brackets.getStart(), index)).findAny();
         }
-        return endIndex + brackets.getEnd().length();
-    }
 
-    private int skipLineComment(int startIndex, CommentBrackets brackets) {
-        int endIndex = source.indexOf('\n', startIndex + brackets.getStart().length());
-        if (endIndex < 0) {
-            return source.length();
-        }
-        return endIndex + 1;
+        private String source;
+        private final Map<String, Matcher> matchers = new HashMap<>();
+        private final Map<Integer, Integer> skips = new HashMap<>();
     }
-
-    private Optional<CommentBrackets> findCommentBrackets(int index) {
-        return grammar.getCommentBrackets().stream().filter(brackets -> source.startsWith(brackets.getStart(), index)).findAny();
-    }
-
-    private String source;
-    private final Map<String, Matcher> matchers = new HashMap<>();
-    private final Map<Integer, Integer> skips = new HashMap<>();
 
     private final Grammar grammar;
 
